3e312edfbbd6caedcac609c1204f12d9596c887c
Shamvi <shamvii@users.noreply.github.com>
2017-07-17 22:01:08 -0400

    Add files via upload
    JS Basic Exercise!

---
+++ stalker.js
@@ -0,0 +1,5 @@
+var firstname = prompt("What is your first name?");
+var lastname = prompt("What is your last name?");
+var age = prompt("What is your age?");
+console.log("Full name is: " + firstname + "" + lastname);
+console.log("Age is : " + age);
